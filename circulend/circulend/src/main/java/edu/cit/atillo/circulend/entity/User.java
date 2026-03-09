b98f761bcb3c9208eda1487607ab53f66bf33d16
Ronel <atilloronel14@gmail.com>
2026-03-09 21:40:27 +0800

    Modified the entity to align its columns or fields with the Created ERD

---
+++ circulend/circulend/src/main/java/edu/cit/atillo/circulend/entity/User.java
@@ -0,0 +1,52 @@
+package edu.cit.atillo.circulend.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false)
+    @JsonIgnore
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String role; // "BORROWER" or "ADMIN"
+
+    @Column(name = "auth_provider", length = 20)
+    private String authProvider;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        if (createdAt == null) {
+            createdAt = LocalDateTime.now();
+        }
+    }
+
+
+
+}
