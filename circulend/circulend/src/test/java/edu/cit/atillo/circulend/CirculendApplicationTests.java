b9ef2605680f62b81d306d0fea5d270bdb4bd025
Ronel <atilloronel14@gmail.com>
2026-03-09 21:46:28 +0800

    Commit all changes

---
+++ circulend/circulend/src/test/java/edu/cit/atillo/circulend/CirculendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.atillo.circulend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CirculendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
