4b12a6cab7b740051a863bb1daa6b2968ee93d7a
Ronel <atilloronel14@gmail.com>
2026-03-09 21:45:29 +0800

    Commited Service: AuthService - in this file we can see the implementation of all our codes, the logic

---
+++ circulend/circulend/src/main/java/edu/cit/atillo/circulend/service/AuthService.java
@@ -0,0 +1,57 @@
+package edu.cit.atillo.circulend.service;
+
+import edu.cit.atillo.circulend.dto.*;
+import edu.cit.atillo.circulend.entity.User;
+import edu.cit.atillo.circulend.repository.UserRepository;
+import edu.cit.atillo.circulend.security.TokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    @Autowired
+    private final UserRepository userRepo;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenProvider tokenProvider;
+
+    public AuthService(UserRepository userRepo,
+                       PasswordEncoder passwordEncoder,
+                       TokenProvider tokenProvider) {
+        this.userRepo = userRepo;
+        this.passwordEncoder = passwordEncoder;
+        this.tokenProvider = tokenProvider;
+    }
+
+    public LoginDataDTO login(LoginRequestDTO dto){
+        User user = userRepo.findByEmail(dto.getEmail()).
+                orElseThrow(() -> new RuntimeException("User not found"));
+        if(!passwordEncoder.matches(dto.getPassword(), user.getPassword())){
+            throw new RuntimeException("Invalid Password!");
+        }
+        String token = tokenProvider.createToken(user.getUserId(), user.getRole());
+        return new LoginDataDTO(UserResponseDTO.fromUser(user), token, null);
+    }
+
+    public User registration(RegisterDTO dto){
+        if(userRepo.existsByEmail(dto.getEmail())){
+            throw new RuntimeException("Email Already Exists!");
+        }
+        User user = new User();
+        user.setFirstName(dto.getFirstName());
+        user.setLastName(dto.getLastName());
+        user.setEmail(dto.getEmail());
+        user.setPassword(passwordEncoder.encode(dto.getPassword()));
+        user.setRole("BORROWER");
+        user.setAuthProvider("LOCAL");
+
+        return userRepo.save(user);
+    }
+
+    public User getUserFromToken(String token) {
+        Long userId = tokenProvider.getUserIdFromToken(token);
+        return userRepo.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+}
