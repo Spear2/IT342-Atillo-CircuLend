8cbbc6f7e8979fdcc778e51733322b4e9d36d312
Ronel <atilloronel14@gmail.com>
2026-03-09 21:45:47 +0800

    Added all the changes

---
+++ circulend/circulend/src/main/java/edu/cit/atillo/circulend/CirculendApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.atillo.circulend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CirculendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CirculendApplication.class, args);
+	}
+
+}
