995d00e5c23649864700d9aeefc75fc6d7d39ac8
Ronel <atilloronel14@gmail.com>
2026-05-09 10:11:55 +0800

    feat(user): added a user management in admin role, so that admin can fully monitor accountability

--- web/my-react-app/src/AdminDashboardRoutes.jsx
+++ web/my-react-app/src/AdminDashboardRoutes.jsx
@@ -6,6 +6,7 @@ import AdminTransactionsPage from "./pages/Admin/Transactions/AdminTransactionsP
 import AdminUsersPage from "./pages/Admin/Users/AdminUsersPage";
 import AdminItemDetailPage from "./pages/Admin/Inventory/AdminItemDetailPage";
 import AdminAuditLogsPage from "./pages/Admin/AuditLogs/AdminAuditLogsPage";
+import UserDetailPage from "./pages/Admin/Users/UserDetailPage";
 
 export default function AdminDashboardRoutes() {
   return (
@@ -17,6 +18,7 @@ export default function AdminDashboardRoutes() {
       <Route path="/logs" element={<AdminAuditLogsPage />} />
       <Route path="*" element={<Navigate to="/admin" replace />} />
       <Route path="/items/:id" element={<AdminItemDetailPage />} />
+      <Route path="/users/:id" element={<UserDetailPage />} />
     </Routes>
   );
 }
